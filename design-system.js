72a8a99a46c744eaec0dfdf5c72a4f28cb0a5cf1
abhiram-shaji <122574760+abhiram-shaji@users.noreply.github.com>
2024-02-25 01:57:08 -0800

    Update logic for active page

--- design-system.js
+++ design-system.js
@@ -4,27 +4,6 @@ function showMenu() {
     document.querySelector('.close').classList.toggle('show');
 }
 
-// Get the current URL path
-var path = window.location.pathname;
-
-// Get the filename from the URL path
-var page = path.split("/").pop();
-
-// Get the link elements
-var aboutLink = document.getElementById("about-link");
-var demoLink = document.getElementById("demo-link");
-
-// Switch case to add 'active' class to the corresponding link based on the page
-switch (page) {
-  case "about-me.html":
-    aboutLink.classList.add("active");
-    break;
-  case "demo-site.html":
-    demoLink.classList.add("active");
-    break;
-  default:
-    break;
-}
 
 
 
