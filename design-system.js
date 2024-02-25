4c846de4f94fb287aa48bb8f859eacc4c6ba09c1
abhiram-shaji <122574760+abhiram-shaji@users.noreply.github.com>
2024-02-25 01:06:37 -0800

    Add the logic to highlight active page
    Also copies the navbar code to about-me page

--- design-system.js
+++ design-system.js
@@ -3,3 +3,20 @@ function showMenu() {
     document.querySelector('.menu').classList.toggle('hide');
     document.querySelector('.close').classList.toggle('show');
 }
+
+// Get the current URL path
+var path = window.location.pathname;
+
+// Get the filename from the URL path
+var page = path.split("/").pop();
+
+// Get the link elements
+var aboutLink = document.getElementById("about-link");
+var demoLink = document.getElementById("demo-link");
+
+// Add 'active' class to the corresponding link based on the page
+if (page === "about-me.html") {
+  aboutLink.classList.add("active");
+} else if (page === "demo-site.html") {
+  demoLink.classList.add("active");
+}
