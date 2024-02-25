39521f925efdba62f0dac427770a275da1244fbd
abhiram-shaji <122574760+abhiram-shaji@users.noreply.github.com>
2024-02-24 23:50:38 -0800

    Apply styles to navigation elements
    Added toggleBox HTML structure with menu and close icons inside anchor tag

--- design-system.js
+++ design-system.js
@@ -1,8 +1,3 @@
-document.getElementById('hamburger-btn').addEventListener('click', function () {
-    var menu = document.getElementById('menu');
-    if (menu.classList.contains('menu--active')) {
-        menu.classList.remove('menu--active');
-    } else {
-        menu.classList.add('menu--active');
-    }
-});
+function showMenu() {
+    document.querySelector()
+}
