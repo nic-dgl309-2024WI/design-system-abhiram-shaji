99851acc819c31d30a9e5aa21d18ba704283ecd2
abhiram-shaji <122574760+abhiram-shaji@users.noreply.github.com>
2024-03-08 00:40:43 -0800

    Update sidebar property to ensure responsiveness

--- design-system.js
+++ design-system.js
@@ -4,3 +4,25 @@ function showMenu() {
     document.querySelector('.close').classList.toggle('show');
 }
 
+function showMenu() {
+    var body = document.body;
+    var navigation = document.querySelector('.navigation');
+    var menuIcon = document.querySelector('.menu');
+    var closeIcon = document.querySelector('.close');
+
+    // Toggle navigation menu visibility
+    navigation.classList.toggle('active');
+
+    // Check if the navigation menu is active
+    if (navigation.classList.contains('active')) {
+        // Menu is active, disable body scroll and show close icon
+        body.style.overflow = 'hidden';
+        menuIcon.style.display = 'none'; // Hide the hamburger menu icon
+        closeIcon.style.display = 'block'; // Show the close icon
+    } else {
+        // Menu is not active, enable body scroll and show hamburger menu icon
+        body.style.overflow = '';
+        menuIcon.style.display = 'block'; // Show the hamburger menu icon
+        closeIcon.style.display = 'none'; // Hide the close icon
+    }
+}
